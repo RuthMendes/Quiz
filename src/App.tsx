60ac99fdcf01e5d3dd488be8e963fe54ce1a2581
rmenga <2022111510051@iesp.edu.com.br>
2024-06-02 18:08:05 -0300

    completo

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useMemo } from "react";
+import { useGlobalProvider } from "hooks";
+import { CreateQuiz, Loader, Quiz, Results } from "components";
+import { View } from "types";
 
 function App() {
-  const [count, setCount] = useState(0)
+    const { showLoader, view } = useGlobalProvider();
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    const renderView = useMemo(() => {
+        switch (view) {
+            case View.Quiz:
+                return <Quiz />;
+            case View.Done:
+                return <Results />;
+            default:
+                return <CreateQuiz />;
+        }
+    }, [view]);
+
+    return (
+        <>
+            <div className="container is-max-desktop py-4">
+                <h1 className="title has-text-centered">
+                    Quiz
+                </h1>
+                {renderView}
+            </div>
+            {showLoader && <Loader />}
+        </>
+    );
 }
 
-export default App
+export default App;
